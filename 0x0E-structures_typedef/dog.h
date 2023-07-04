98d49164c33af12c371037ceaa02f7036e1e224c
Ahmed LAZAAR <102024029+AhmedLAZAAR@users.noreply.github.com>
2023-07-04 20:35:00 +0200

    Update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,23 +1,27 @@
-#ifndef DOG_H
-#define DOG_H
+#ifndef DOGH
+#define DOGH
+
 /**
- * struct dog - list dates about pet
- * @name: pet name
- * @age: pet age
- * @owner: pet owner name
+ * struct dog - a dog
+ *
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog;
+
+typedef dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-/**
- * dog - rename strucut with typedef
- */
-typedef struct dog dog_t;
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-#endif /* DOG_H */
+
+int _putchar(char c);
+
+#endif
